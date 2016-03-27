1d60c4ccf1b3de7d49a8d085909bcb920e33dc8d
Chang Si Yuan <changsiyuan1999@gmail.com>
2016-03-27 19:52:48 +0800

    pluggableHID library for leonardo compiles
    testers please volunteer yourselves

---
+++ leoo/text/text.ino
@@ -0,0 +1,12 @@
+#include "iivx_leo.h"
+iivxReport_t report; 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  iivx.setState(&report);
+}
